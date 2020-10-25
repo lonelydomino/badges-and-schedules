570da7b0567876a1e211caf0dc7ad89968c7ea7b
Michael Martinez <zeroeclipse@live.com>
2020-10-24 23:07:45 -0700

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,10 +5,8 @@ end
 
 def batch_badge_creator(names)
     badge_names = []
-    i = 0
-    names.each do |name|
-        badge_names[i] = "Hello, my name is #{name}."
-        i += 1
+    names.each_with_index do |name, index|
+        badge_names[index] = "Hello, my name is #{name}."
     end
 return badge_names
 end
@@ -16,10 +14,8 @@ end
 
 def assign_rooms(names)
     rooms = []
-    i = 0
-    names.each do |name|
-        rooms[i] = "Hello, #{name}! You'll be assigned to room #{i + 1}!"
-        i += 1
+    names.each_with_index do |name, index|
+        rooms[index] = "Hello, #{name}! You'll be assigned to room #{index + 1}!"
     end
 return rooms
 end
